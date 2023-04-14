af2342f0a5c7e3c472b569f62fd85be585d13cda
Elvir Vlahovljak <elvirvlahovljak@gmail.com>
2023-04-14 14:39:35 +0200

    FEATURE/ElvaRivle/DfWCIPVU
    --------------------------------------------------------------------
     MAIN
     * successfully connected to MongoDB
     * created User model
     * seeded default users to DB
    --------------------------------------------------------------------
     OTHER
     * added all necessary packages
     * loading connection string from .env file
     * listening and logging mongoose events
    --------------------------------------------------------------------

---
+++ db/connection.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose successfully connected to DB');
+});
+
+mongoose.connection.on('error', function (err) {
+    console.error('Mongoose error:', err.stack);
+    process.exit(1);
+});
+
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected from DB');
+});
+
+
+//is it neccessary to capture SIGTERM?
+process.on('SIGINT', function () {
+    mongoose.disconnect()
+        .then(() => {
+            console.log("App terminating! Mongoose connection to DB closed.")
+            process.exit(0);
+        })
+        .catch(err => {
+            console.log("App terminating! Mongoose connection to DB NOT closed: " + err.stack)
+        })
+});
+
+
+mongoose.connect(process.env.DB_CONNECTION_STRING)
+
+module.exports = mongoose
