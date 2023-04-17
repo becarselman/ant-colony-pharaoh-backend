e745442abcaa207c9a33737591218bf4de693ad6
Elvir Vlahovljak <46258494+ElvaRivle@users.noreply.github.com>
2023-04-17 12:06:25 +0200

    FEATURE/ElvaRivle/DfWCIPVU (#4)
    * FEATURE/ElvaRivle/DfWCIPVU
    
    --------------------------------------------------------------------
     MAIN
     * successfully connected to MongoDB
     * created User model
     * seeded default users to DB
    --------------------------------------------------------------------
     OTHER
     * added all necessary packages
     * loading connection string from .env file
     * listening and logging mongoose events
    --------------------------------------------------------------------
    
    * FEATURE/ElvaRivle/DfWCIPVU
    
    --------------------------------------------------------------------
     MAIN
     * seeder converted to script
     * README added
    --------------------------------------------------------------------
     OTHER
     * changed default users for seeding
    --------------------------------------------------------------------

---
+++ db/seed.js
@@ -0,0 +1,43 @@
+const mongoose = require("./connection")
+const User = require("./models/User")(mongoose)
+const bcrypt = require("bcrypt")
+
+const users = [
+    {
+        "email": "pharaohadmin1@gmail.com",
+        "password": bcrypt.hashSync("PharaohPassword1", 10)
+    },
+    {
+        "email": "pharaohadmin2@gmail.com",
+        "password": bcrypt.hashSync("PharaohPassword2", 10)
+    }
+]
+
+async function seed() {
+
+    console.log("Attempting to seed...")
+
+    const numOfDefaultUsersInDB = await User.count({
+        email: {
+            $in: [
+                "pharaohadmin1@gmail.com",
+                "pharaohadmin2@gmail.com"
+            ]
+        }
+    })
+
+    //don't seed users if they already exist in DB
+    if (numOfDefaultUsersInDB == 0) {
+        await User.insertMany(users)
+    }
+
+}
+
+seed()
+    .then(() => {
+        console.log("Default users seeded successfully")
+        mongoose.disconnect()
+    })
+    .catch(err => {
+        console.log("Error occured whiled seeding: " + err.stack)
+    })
