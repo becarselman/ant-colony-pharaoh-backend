d5f8e4ba688fcd3d7a31d717f2ec5f0929113fec
meliha01 <kasapovic.m@hotmail.com>
2023-04-24 23:39:35 +0200

    SENDING EMAIL BACKEND
    --------------------------------------------------------------------
     MAIN
     * fixing errors in controllers/sendemail.js
    --------------------------------------------------------------------
     OTHER
     * ////
    --------------------------------------------------------------------

--- controllers/sendemail.js
+++ controllers/sendemail.js
@@ -2,10 +2,10 @@ const sendMailService = require('../services/sendemail')
 const errors = require("../configuration/errors")
 
 exports.sendEmail = async (req, res) => {
-    const { email } = req.body
+    const { email, templateName } = req.body
 
     try {
-        await sendMailService.sendEmail(email)
+        await sendMailService.sendEmail(email, templateName)
         res.status(200).send("OK");
     } catch (error) {
         res.status(500).json({ error: errors.FAILED_TO_SEND_EMAIL });
