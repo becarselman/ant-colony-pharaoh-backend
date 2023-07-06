1eff6bfc532ef1c0bd4409e4cffe75ffacb4a1e8
Elvir Vlahovljak <elvirvlahovljak@gmail.com>
2023-07-06 21:54:03 +0200

    ElvaRivle/Misc/LoginReturnData
    --------------------------------------------------------------------
     MAIN
     * on login return name and surname as well
    --------------------------------------------------------------------
     OTHER
     * /
    --------------------------------------------------------------------
     TODO
     * /

--- controllers/login.js
+++ controllers/login.js
@@ -3,11 +3,13 @@ const loginService = require("../services/login")
 
 const loginUser = async (req, res) => {
     try {
-        const { userId, token } = await loginService.loginUser(req.body)
+        const { userId, token, name, surname } = await loginService.loginUser(req.body)
 
         return res.status(200).json({
             userId: userId,
-            token: token
+            token: token,
+            name, 
+            surname
         })
     }
     catch (err) {
